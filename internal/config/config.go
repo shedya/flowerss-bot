ff73d5ea07836ea736f472d0ff1c240aa67956d3
SHED <53966497+shedya@users.noreply.github.com>
2024-04-13 05:17:21 +0800

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -65,11 +65,9 @@ const (
 	defaultMessageTpl     = `
  <b>{{.ContentTitle}}</b>
  
- {{ if .PreviewText }}
- 
-{{.PreviewText}}
+{{ if .PreviewText }}{{.PreviewText}}{{- end}}
 
-{{- end}}{{if .EnableTelegraph}}
+{{if .EnableTelegraph}}
 {{.ContentTitle}} <a href="{{.RawLink}}">查看原文</a> | <a href="{{.TelegraphURL}}">Telegraph</a>
 {{- else }}
 <a href="{{.RawLink}}">查看原文</a>
@@ -79,11 +77,7 @@ const (
 	defaultMessageMarkdownTpl = `
 ** {{.ContentTitle}} **
  
-{{ if .PreviewText }}
- 
-{{.PreviewText}}
-
-{{- end}}
+{{ if .PreviewText }}{{.PreviewText}}{{- end}}
 
 {{if .EnableTelegraph}}
 {{.ContentTitle}} [查看原文]({{.RawLink}} |  [Telegraph]({{.TelegraphURL}}))
